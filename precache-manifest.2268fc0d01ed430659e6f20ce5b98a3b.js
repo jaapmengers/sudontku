89896d615637fd6dc6b630db56f03a95c8cc1d19
Jaap Mengers <jaapm@q42.nl>
2020-07-05 11:18:12 +0200

    Updates

---
+++ precache-manifest.2268fc0d01ed430659e6f20ce5b98a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c710183af55140e86da46c421fdf4cad",
+    "url": "/sudontku/index.html"
+  },
+  {
+    "revision": "d366e09119f99e3741e6",
+    "url": "/sudontku/static/css/main.f0ce3bc3.chunk.css"
+  },
+  {
+    "revision": "69666fc2fa570a6cb71a",
+    "url": "/sudontku/static/js/2.7f55dbab.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/sudontku/static/js/2.7f55dbab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d366e09119f99e3741e6",
+    "url": "/sudontku/static/js/main.c6f3c871.chunk.js"
+  },
+  {
+    "revision": "98d51294dcc024b1419e",
+    "url": "/sudontku/static/js/runtime-main.a05f214b.js"
+  }
+]);
